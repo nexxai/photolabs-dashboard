bec2fe735e2a77304e29816a553dae213b8db24c
JT Smith <nexxai@gmail.com>
2023-10-09 15:57:47 -0600

    Binding to this

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,18 +2,66 @@ import React, { Component } from "react";
 
 import classnames from "classnames";
 import Loading from "./Loading";
+import Panel from "./Panel";
+
+const data = [
+  {
+    id: 1,
+    label: "Total Photos",
+    value: 10,
+  },
+  {
+    id: 2,
+    label: "Total Topics",
+    value: 4,
+  },
+  {
+    id: 3,
+    label: "User with the most uploads",
+    value: "Allison Saeng",
+  },
+  {
+    id: 4,
+    label: "User with the least uploads",
+    value: "Lukas Souza",
+  },
+];
 
 class Dashboard extends Component {
-  state = { loading: false };
+  state = { loading: false, focused: null };
+
+  selectPanel(id) {
+    this.setState({
+      focused: id,
+    });
+  }
 
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused,
+    });
 
     if (this.state.loading) {
       return <Loading />;
     }
 
-    return <main className={dashboardClasses} />;
+    const panels = (
+      this.state.focused
+        ? data.filter((panel) => this.state.focused === panel.id)
+        : data
+    ).map((panel) => {
+      return (
+        <Panel
+          key={panel.id}
+          id={panel.id}
+          label={panel.label}
+          value={panel.value}
+          onSelect={() => this.selectPanel(panel.id)}
+        />
+      );
+    });
+
+    return <main className={dashboardClasses}>{panels}</main>;
   }
 }
 
